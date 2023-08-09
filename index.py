6a7e2c564e1e381987f5534065abd0912a3d3f87
David Keipert <hello@davidkeipert.me>
2023-08-09 01:57:28 -0500

    username filtering/banning

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import websocket
 import requests
 import json
 import keys
+import asyncio
 from concurrent.futures import ProcessPoolExecutor
 
 from perspective import perspective_ban
@@ -14,7 +15,8 @@ websocket_url = keys.OWNCAST_WEBSOCKET
 
 log = open("deleted.txt", "a")
 
-words = ['nigger', 'faggot', 'fag', 'http', 'cum']
+words = ['nigger', 'faggot', 'fag', 'http', 'cum', '123test', 'kaylor']
+banned_names = ["卐", "ᛋ", "nigg", "í", 'é', "gas", "jew", "test", "ttt"]
 
 chat_name = {"displayName": "Vigilante"}
 chat_response = requests.post(owncast_url + "/api/chat/register", json=chat_name)
@@ -32,6 +34,7 @@ def delete_message(id, text):
     )
     if delete_response.json()["success"] == True:
         log.write("\n" + text)
+        print("DELETED: " + text)
         removed += 1
         return True
     else:
@@ -42,11 +45,30 @@ def delete_message(id, text):
 # makes calls to delete_message and perspective.py
 async def automod(message):
     msg = json.loads(message)
+    if msg["type"] == "NAME_CHANGE":
+        print("NAME CHANGE DETECTED")
+        ban = False
+        name = msg["user"]["displayName"].lower()
+        user_id = msg["user"]["id"]
+        ban_request = {"userId": user_id, "enabled": False}
+        for word in banned_names:
+            if word in name:
+                ban = True
+            
+        if ban == True:
+            banned = requests.post(owncast_url + "/api/chat/users/setenabled?accessToken=" + mod_token, json = ban_request)
+            if banned.json()["success"] == True:
+                print("____________BANNED USER + " + name + "!____________")
+            else:
+                print("_________BAN FAILED")
+                print(banned.json())
+
     
     if msg["type"] == "CHAT":
         chat_text = msg["body"]
         chat_id = msg["id"]
         name = msg["user"]["displayName"]
+        lower_name = name.lower()
         lower_chat = chat_text.lower()
         print(name + ": " + chat_text)
 
@@ -58,15 +80,15 @@ async def automod(message):
             if word in chat_text:
                 delete_message(chat_id, chat_text)
 
-        if "卐" in name:
+        if "卐" in name or "ᛋ" in name or "nigg" in lower_name or "NÍG"in name or "NIGGéR" in name or "gas" in lower_name or "jew" in lower_name:
             delete_message(chat_id, chat_text)
 
 removed = 0
 
+
 # websocket app on_message function
 def on_message(ws_app, message):
-    with ProcessPoolExecutor as exe:
-        exe.submit(automod(message))
+    asyncio.run(automod(message))
    
 
 
